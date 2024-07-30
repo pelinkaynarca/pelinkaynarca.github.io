d0fdca37e2fa177d00094b9a5ba1b3279e8a5820
Pelin <kaynarcapelin@hotmail.com>
2024-07-30 13:08:37 +0300

    feat: add svg components

---
+++ src/components/SvgComponents/NavbarLinkedInLogo.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const NavbarLinkedInLogo = () => (
+    <svg
+        width="30px"
+        height="30px"
+        viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#FFFFFF"
+        stroke="#FFFFFF"
+        strokeWidth="0.00016"
+    >
+        <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+        <g
+            id="SVGRepo_tracerCarrier"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+        <g id="SVGRepo_iconCarrier">
+            {" "}
+            <path
+                className="group-hover:fill-accent2"
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M3 1a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2V3a2 2 0 00-2-2H3zm1.102 4.297a1.195 1.195 0 100-2.39 1.195 1.195 0 000 2.39zm1 7.516V6.234h-2v6.579h2zM6.43 6.234h2v.881c.295-.462.943-1.084 2.148-1.084 1.438 0 2.219.953 2.219 2.766 0 .087.008.484.008.484v3.531h-2v-3.53c0-.485-.102-1.438-1.18-1.438-1.079 0-1.17 1.198-1.195 1.982v2.986h-2V6.234z"
+                fill="#FFFFFF"
+            />{" "}
+        </g>
+    </svg>
+);
+
+export default NavbarLinkedInLogo;
